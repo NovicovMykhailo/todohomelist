2cf40c2c5ffc4ed04b1eca7ed6f72548014bed0b
NovicovMykhailo <mikenovicov@gmail.com>
2023-05-16 13:30:00 +0300

    add adaptive

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -10,14 +10,16 @@ export default function Filter({ onChange, filterValue }) {
         onChange={onChange}
         value={filterValue}
       />
-      <label>
-        Mike
-        <input type="checkbox" name="filter" id="Mike" onChange={onChange} />
-      </label>
-      <label>
-        Kate
-        <input type="checkbox" name="filter" id="Kate" onChange={onChange} />
-      </label>
+      <div className={css.checkContainer}>
+        <label>
+          Mike
+          <input type="checkbox" name="filter" id="Mike" onChange={onChange} />
+        </label>
+        <label>
+          Kate
+          <input type="checkbox" name="filter" id="Kate" onChange={onChange} />
+        </label>
+      </div>
     </div>
   );
 }
